f4dc9b0f4b841edefcb0def06126067fa508c7a7
LeonidV <lvarchenko99@gmail.com>
2021-03-09 21:45:08 +0200

    added: 1.New programm for separate cryptokeys by algorithms  2. process data comunication

--- find-algorythm.cpp
+++ find-algorythm.cpp
@@ -1,7 +1,36 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
+
+
+
+void pipe_out(char* result);
 
 int main(int argc, char* argv[]){
-  printf(argv[1]);
-  return EXIT_SUCCESS;
+  if ( argv[1] == std::string("--hello") )
+    printf("hello i'm your crypto algorithm finder");
+
+  else if ( argv[1]== std::string("-k") )
+      pipe_out( argv[2] );
+
+  else if ( argv[1] == std::string("-h") || argv[1] == std::string("--help") )
+    printf("manual must be here");
+
+  else {
+    printf("nothing to do or you type wrong parameter. Read manual by write -h");
+    return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+
+
+}
+
+
+void pipe_out(char* result){
+
+
+  fprintf (stdout,result);
+
 }

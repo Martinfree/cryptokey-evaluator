e022f5a715eaece6c0a392646af13f667b75ea4e
LeonidV <lvarchenko99@gmail.com>
2021-05-11 22:00:02 +0300

    modified algorithm

--- src/api.cpp
+++ src/api.cpp
@@ -10,28 +10,47 @@ In this part you may see:
   main function normally call with parametrs place -h to help
 */
 
-int crypto_finder(char* i);
+void InitApp(char *app, char* args);
 
 
 int main (int argc, char* argv[]){
     while (argc == 0) return EXIT_FAILURE;
 
-    if ( argv[1] == std::string("--hello") || argv[1] == std::string("--help") )
-      printf("hello i'm your api");
-
-    else if (argv[1] == std::string("--test") && argv[2]) //test some features
+    if (
+        argv[1] == std::string("--hello") || argv[1] == std::string("--help")
+        )
     {
-        AppThread FindExe(std::string("find.exe ").c_str(), std::string("-k " + std::string(argv[2])).c_str());
-        FindExe.FormatStr();
+        printf("hello i'm your api you can read manual by -h option");
+    }
+
+    else if (
+        argv[1] == std::string("--test") && argv[2]
+        ) //test some features
+    {
+        //TEST
+        //AppThread FindExe(std::string("find.exe ").c_str(), std::string("-k " + std::string(argv[2])).c_str());
+        //FindExe.FormatStr();
         //crypto_finder(argv[2]); 
     }  
-    else if (argv[1] == std::string("-f")) //find algorithm
+    else if (
+        (argv[1] == std::string("-f") || argv[1] == std::string("--find-whoami")) && argv[2]
+        ) //find algorithm
     {
-
-        //crypto_finder(argv[2]); 
+        printf("i don't know now");
     }
-    else if ( argv[1] == std::string("-h") )
-      printf("manual must be here");
+    else if (
+        (argv[1] == std::string("-fast") || argv[1] == std::string("--fast-check")) && argv[2]
+        ) //find algorithm
+    {
+        printf("fast check");
+    }
+
+    else if (
+        argv[1] == std::string("-h")
+        )
+    {
+        printf("manual must be here");
+    }
 
     else {
       printf("nothing to do or you type wrong parameter. Read manual by write -h");
@@ -41,28 +60,6 @@ int main (int argc, char* argv[]){
   return EXIT_SUCCESS;
 }
 
-// useless function
-int crypto_finder(char* i){
-    printf("I'm alive");
-  //std::string call_buffer = "find.exe -k ";
-  //call_buffer += std::string(i);
-
-  //if ( (Pcrypto_finder = popen( call_buffer.c_str(),"r")) == NULL )
-  //  return EXIT_FAILURE;
-
-  /*else{
-
-    std::string result = "";
-    char* buffer = new char [MAXIMUM_BUFFER];
-     // use buffer to read and add to result
-     while(fgets(buffer,MAXIMUM_BUFFER,Pcrypto_finder)){
-       puts(buffer);
-      }
-      pclose(Pcrypto_finder);
-
-      delete buffer;
-    
-  return EXIT_SUCCESS;
-}*/
-    return EXIT_SUCCESS;
+void InitApp(char* app, char* args) {
+    printf("i useless now");
 }

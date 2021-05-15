d290db10cac099fdffbb084a7c7e548ea1dffaef
LeonidV <lvarchenko99@gmail.com>
2021-05-15 17:41:40 +0300

    fast-check is like ready

--- src/fast-check.cpp
+++ src/fast-check.cpp
@@ -1,17 +1,23 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <cmath>
 #include <string>
 #include <iostream>
+#include <iomanip> 
+#include <sstream>
 #include <fstream>
 
 
+
 //
-// Program for search password in wordlist
+// Program for search password in wordlist and calculate time to hack
 //
 
 void pipe_out(std::string result);
-std::string FastCheckShow(std::string str, std::ifstream& wordlist);
+std::string FastCheckShowPass(std::string str, std::ifstream& wordlist, std::string file_name);
+std::string AnalyzePass(std::string str);
+std::string AnalyzeSecText(std::string str);
 
 int main(int argc, char* argv[]) {
     if (
@@ -21,17 +27,43 @@ int main(int argc, char* argv[]) {
 
     else if (
         (argv[1] == std::string("-s") || argv[1] == std::string("--single")) && argv[2]
-        ) // dafult wordlists
+        ) // default wordlists
     {
+        char path[256];
+        getcwd(path, 256);
         std::ifstream file("include/rockyou.txt", std::ifstream::in);
-        pipe_out(FastCheckShow(std::string(argv[2]), file));
+        pipe_out(FastCheckShowPass(std::string(argv[2]), file, std::string(path) + std::string("/") + std::string(argv[2])));
         file.close();
     }
     else if (
-        argv[1] == std::string("-f") || argv[2]
-        ) // send to me wordlist file 
-        pipe_out(argv[2]);
+        argv[1] == std::string("-f") && argv[2] && argv[3]
+        ) // send to me where wordlist file 
+    {
+        char path[256];
+        getcwd(path, 256);
+        std::ifstream file((std::string(path) + std::string("/") + std::string(argv[2])).c_str(), std::ifstream::in);
+        if (
+            file.is_open()
+            )
+            pipe_out(FastCheckShowPass(std::string(argv[3]), file, std::string(path) + std::string("/") + std::string(argv[2])));
 
+        else
+            printf("No such file or dir:\n", (std::string(path) + std::string("/") + std::string(argv[2])).c_str());
+    }
+    else if (
+        (argv[1] == std::string("--analyze-pass")) && argv[2]
+        )// analyze password
+    {
+        pipe_out(AnalyzePass(argv[2]));
+    }
+    else if (
+        argv[1] == std::string("-a") || argv[1] == std::string("--analyze") && argv[2]
+        )// analyze only secret text (second arg is posiible algorithm)
+    {
+        
+        pipe_out(AnalyzeSecText(argv[2]));
+    }
+        
     else if (
         argv[1] == std::string("-h") || argv[1] == std::string("--help")
         )
@@ -55,14 +87,85 @@ void pipe_out(std::string result) {
 
 }
 
-std::string FastCheckShow(std::string str, std::ifstream& wordlist)
+std::string FastCheckShowPass(std::string str, std::ifstream& wordlist, std::string file_name)
 {
     std::string line;
     for (unsigned int curLine = 0; std::getline(wordlist, line); curLine++) {
         if (line.find(str) != std::string::npos) {
-            str = std::string("found: ") + str + std::string(" in file: <NONE>");
+            str = std::string("found: ") + str + std::string(" in file: ") + file_name;
             return str;
         }
     }
-    return "ERROR";
+    return "Password Not found in files";
+}
+
+
+std::string AnalyzePass(std::string str)
+{
+    int upper = 0, lower = 0, number = 0, special = 0;
+    long double comb = 0.0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            upper++;
+        else if (str[i] >= 'a' && str[i] <= 'z')
+            lower++;
+        else if (str[i] >= '0' && str[i] <= '9')
+            number++;
+        else
+            special++;
+    }
+    
+    
+    if (
+        (number != 0) && (upper+ lower+ special == 0)
+        )
+        comb = pow(10, str.length());
+    
+    else if (
+        (lower != 0 && upper+ number+ special == 0) || (upper != 0 && lower+ number+ special == 0)
+        )
+        comb = pow(26, str.length());
+   
+    else if (
+        (number, lower != 0 && special+ upper == 0) || (number, upper != 0 && special+ lower == 0)
+        )
+        comb = pow(36, str.length());
+
+    else if (
+        (upper != 0 && lower != 0) && (number+ special == 0)
+        )
+        comb = pow(52, str.length());
+    
+    else if (
+        number != 0 && upper != 0 && lower != 0 && special == 0
+        )
+        comb = pow(62, str.length());
+
+    else if (
+        special != 0 && lower != 0 && upper != 0 && number == 0
+        )
+        comb = pow(84, str.length());
+    
+    else if (
+        number != 0 && lower != 0 && upper != 0 && special != 0
+        )
+        comb = pow(94, str.length());
+
+    else comb = pow(32, str.length());
+    
+    std::stringstream result;
+    result << std::string("Password length: ") << std::fixed << std::setprecision(0) << str.length() << std::string("\n")
+        << std::string("Uppercase: ") << upper << std::string("\n")
+        << std::string("Lowerlacase: ") << lower << std::string("\n")
+        << std::string("Numbers: ") << number << std::string("\n")
+        << std::string("Special: ") << special << std::string("\n")
+        << std::string("Possible combinations: ") << comb << std::string("\n");
+    return result.str();
+}
+
+std::string AnalyzeSecText(std::string str)
+{
+    
+    return "No";
 }
